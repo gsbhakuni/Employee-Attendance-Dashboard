7cfae7053a996e7f7f0abf72f9d49406a956411f
Gaurav Singh Bhakuni <gauravbhakuni666@gmail.com>
2026-08-12 06:34:21 +0530

    Add app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import util
+st.set_page_config(page_title="Employee Attendance Dashboard", layout="wide")
+
+st.title("Employee Attendance Analytics and Prediction Dashboard")
+
+st.write(
+    "Upload the attendance dataset to view analytics and predict employee attendance."
+)
