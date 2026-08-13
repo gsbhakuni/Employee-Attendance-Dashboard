e8c1a80fd2dacb189781b57baffb28254240b82a
Gaurav Singh Bhakuni <gauravbhakuni666@gmail.com>
2026-08-13 05:40:56 +0530

    Added status distribution graph

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 import util
 import analytics
 st.set_page_config(page_title="Employee Attendance Dashboard", layout="wide")
@@ -27,3 +28,19 @@ if uploaded_file is not None:
     col4.metric("Absent", absent_count)
     col5.metric("Leave", leave_count)
     col6.metric("Attendance %", f"{attendance_percentage}%")
+
+    st.subheader("Attendance Status Distribution")
+
+    status_counts = df["status"].value_counts()
+
+    fig, ax = plt.subplots()
+
+    ax.bar(status_counts.index, status_counts.values)
+
+    ax.set_xlabel("Status")
+    ax.set_ylabel("Count")
+    ax.set_title("Attendance Distribution")
+
+    plt.xticks(rotation=20)
+
+    st.pyplot(fig)
