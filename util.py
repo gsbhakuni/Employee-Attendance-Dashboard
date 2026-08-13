19d28bebf1b8f9252d494b5492eb4bf352646929
Gaurav Singh Bhakuni <gauravbhakuni666@gmail.com>
2026-08-13 06:09:45 +0530

    Add function for employee- wise attendance calculation

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ def load_data(uploaded_file):
     df["date"] = pd.to_datetime(df["date"]).dt.date
     return df
 
-def summary(df):
+def dep_summary(df):
     department_summary = (
         df.groupby("department")["status"].value_counts().unstack(fill_value=0)
     )
@@ -20,3 +20,19 @@ def summary(df):
         )
     ).round(2)
     return department_summary
+
+def emp_summary(df):
+    employee_summary = (
+        df.groupby("employee_id")["status"].value_counts().unstack(fill_value=0)
+    )
+    employee_summary["Attendance%"] = (
+            100
+            * (employee_summary["Present"] + employee_summary["Work From Home"])
+            / (
+                employee_summary["Present"]
+                + employee_summary["Work From Home"]
+                + employee_summary["Absent"]
+                + employee_summary["Leave"]
+            )
+        ).round(2)
+    return employee_summary
